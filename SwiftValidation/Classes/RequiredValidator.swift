0ded851f23b1aa239e2796f9ba6b5d022da71e71
Tom Knapen <tom@failproof.be>
2016-06-10 14:31:35 +0200

    fixes

--- SwiftValidation/Classes/RequiredValidator.swift
+++ SwiftValidation/Classes/RequiredValidator.swift
@@ -9,27 +9,28 @@
 import Foundation
 
 class RequiredValidator: Validator {
-	
+
 	let msg: String?
-	
+
 	init(_ message: String? = nil) {
 		msg = message
 	}
-	
+
 	func message() -> String? {
 		return self.msg
 	}
-	
+
 	func evaluate(str: String?, _ valid: (String? -> Void)?, _ invalid: (String? -> Void)?) -> Void {
-		guard let str = str where str.characters.count > 0 else {
+		guard let subject = str where subject.characters.count > 0 else {
 			if let invalid = invalid {
 				invalid(str)
 			}
+			return
 		}
-		
+
 		if let valid = valid {
-			valid(str)
+			valid(subject)
 		}
 	}
-	
+
 }
